79b4333345e5e369d82578ed19bb1f33b3a266aa
Nchng1 <411428340@qq.com>
2021-01-02 19:30:42 +0800

    方法抽取

--- src/Test.java
+++ src/Test.java
@@ -2,13 +2,21 @@ import java.io.InputStream;
 import java.util.Scanner;
 
 public class Test {
+
+    static int count;
+    static Product proSelected[] = new Product[5];//购物车
+    static Product product;
+    static Product[] products;
+    static String pId;
+    static InputStream proIn;
+    static ReadProductsExcel readProductsExcel;
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) throws ClassNotFoundException {
         boolean flag = true;
         while (flag) {
-            Scanner scanner = new Scanner(System.in);
             System.out.println("请输入用户名：");
             String testName = scanner.next().trim();
-
             System.out.println("请输入密码：");
             String testPassword = scanner.next().trim();
 /*
@@ -19,36 +27,36 @@ public class Test {
             ReadUsersExcel readUsersExcel = new ReadUsersExcel();
             User[] users = readUsersExcel.getAllUsers(in);
 
-            InputStream proIn = Class.forName("Test").getResourceAsStream("/products.xlsx"); // /表示的是classpath
-            ReadProductsExcel readProductsExcel = new ReadProductsExcel();
-            Product[] products = readProductsExcel.getAllProducts(proIn);
+            proIn = Class.forName("Test").getResourceAsStream("/products.xlsx"); // /表示的是classpath
+            readProductsExcel = new ReadProductsExcel();
+            products = readProductsExcel.getAllProducts(proIn);
 
             for (User user : users) {
                 if (testName.equals(user.getName()) && testPassword.equals(user.getPassword())) {
                     System.out.println("登录成功");
+                    flag = false;
                     /*
                     显示商品
                     */
-                    int count = 0;
-                    Product proSelected[] = new Product[5];
                     for (Product p : products) {
                         System.out.print("" + p.getProId());
                         System.out.print("\t\t" + p.getProName());
                         System.out.print("\t\t" + p.getPrice());
                         System.out.println("\t\t" + p.getDecription());
                     }
-                    System.out.println("请输入商品ID：");
-                    String pId=scanner.next();
+                    System.out.println("购买请输入商品ID：");
+                    pId = scanner.next();
                     proIn = Class.forName("Test").getResourceAsStream("/products.xlsx");
 //                    ReadProductsExcel readProductsExcel1 = new ReadProductsExcel();
-                    Product product = readProductsExcel.getProById(pId, proIn);
-                    if ( product!= null) {
+                    product = readProductsExcel.getProById(pId, proIn);
+                    if (product != null) {
                         /*
                         把商品加入购物车
                         */
 //                        proIn = Class.forName("Test").getResourceAsStream("/products.xlsx");
                         proSelected[count++] = product;
                         System.out.println("商品已加入购物车");
+                        System.out.println("");
                     }
                     /*
                     1、查看购物车
@@ -57,49 +65,103 @@ public class Test {
                     2、继续购物
                        显示所有的商品
                     */
-                    System.out.println("查看已购商品请输入1\n继续购物请输入2");
-                    int number = scanner.nextInt();
-                    if(number == 1){
-                        for(Product p : proSelected){
-                                if(p == null){
-                                    break;
-                                }
-                            System.out.print(p.getProId());
-                            System.out.print("\t" + p.getProName());
-                            System.out.print("\t\t" + p.getPrice());
-                            System.out.println("\t\t" + p.getDecription());
+                    while (true) {
+                        System.out.println("查看已购商品请输入1\n继续购物请输入2\n结账请输入3\n退出商城请输入4");
+                        int number = scanner.nextInt();
+                        if (number == 1 || number == 2) {
+                            //调用Mothod类的mothod方法
+                            choose(number);
+//                        for (Product p : proSelected) {
+//                            if (p == null) {
+//                                break;
+//                            }
+//                            System.out.print(p.getProId());
+//                            System.out.print("\t" + p.getProName());
+//                            System.out.print("\t\t" + p.getPrice());
+//                            System.out.println("\t\t" + p.getDecription());
+//                        }
+//                    } else if (number == 2) {
+//                        for (Product p : products) {
+//                            System.out.print("" + p.getProId());
+//                            System.out.print("\t\t" + p.getProName());
+//                            System.out.print("\t\t" + p.getPrice());
+//                            System.out.println("\t\t" + p.getDecription());
+//                        }
+//                        System.out.println("请输入商品ID：");
+//                        pId = scanner.next();
+//                        proIn = Class.forName("Test").getResourceAsStream("/products.xlsx");
+////                        ReadProductsExcel readProductsExcel2 = new ReadProductsExcel();
+//                        Product product1 = readProductsExcel.getProById(pId, proIn);
+//                        if (product1 != null) {
+//                        /*
+//                        把商品加入购物车
+//                        */
+////                        proIn = Class.forName("Test").getResourceAsStream("/products.xlsx");
+//                            proSelected[count++] = product1;
+//                            System.out.println("商品已加入购物车");
+//                        }
+                        } else if (number == 3) {
+                            System.out.println("结账");
+                            break;
+                        } else if (number == 4) {
+                            System.out.println("商城已退出！");
+                            break;
+                        }
+                    }
+                }
+            }
+            if (flag == true)
+                System.out.println("登录失败");
+        }
+    }
 
-                        }
-                    }else if(number == 2){
-                        for (Product p : products) {
-                            System.out.print("" + p.getProId());
-                            System.out.print("\t\t" + p.getProName());
-                            System.out.print("\t\t" + p.getPrice());
-                            System.out.println("\t\t" + p.getDecription());
-                        }
-                        System.out.println("请输入商品ID：");
-                        pId=scanner.next();
-                        proIn = Class.forName("Test").getResourceAsStream("/products.xlsx");
+    //方法抽取
+    public static void choose(int number) throws ClassNotFoundException {
+        /*
+        1、查看购物车
+         （1）购物车是用数组模拟的
+         （2）对数组遍历
+        2、继续购物
+           显示所有的商品
+        */
+        if (number == 1) {
+            //显示已购商品
+            for (Product p : proSelected) {
+                if (p == null) {
+                    break;
+                }
+                System.out.print(p.getProId());
+                System.out.print("\t" + p.getProName());
+                System.out.print("\t\t" + p.getPrice());
+                System.out.println("\t\t" + p.getDecription());
+
+            }
+        } else if (number == 2) {
+            //显示商品
+            for (Product p : products) {
+                System.out.print("" + p.getProId());
+                System.out.print("\t\t" + p.getProName());
+                System.out.print("\t\t" + p.getPrice());
+                System.out.println("\t\t" + p.getDecription());
+            }
+            System.out.println("请输入商品ID：");
+            pId = scanner.next();
+            proIn = Class.forName("Test").getResourceAsStream("/products.xlsx");
 //                        ReadProductsExcel readProductsExcel2 = new ReadProductsExcel();
-                        Product product1 = readProductsExcel.getProById(pId, proIn);
-                        if ( product1!= null) {
+            product = readProductsExcel.getProById(pId, proIn);
+            if (product != null) {
                         /*
                         把商品加入购物车
                         */
 //                        proIn = Class.forName("Test").getResourceAsStream("/products.xlsx");
-                            proSelected[count++] = product1;
-                            System.out.println("商品已加入购物车");
-                        }
-                    }
-
-                    flag = false;
-                    break;
+                if (count < 5) {
+                    proSelected[count++] = product;
+                    System.out.println("商品已加入购物车");
+                    System.out.println("");
+                } else {
+                    System.out.println("购物车已满,无法加入新的商品");
                 }
             }
-            if(flag == true)
-                System.out.println("登录失败");
         }
-
     }
-
 }
